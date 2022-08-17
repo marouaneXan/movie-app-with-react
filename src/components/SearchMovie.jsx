ddb93d6d5aee679ef8ed93563c5f891d39055f54
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-08-17 13:57:15 +0100

    finished

--- src/components/SearchMovie.jsx
+++ src/components/SearchMovie.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import SearchIcon from '../search.svg'
-import {useRef} from 'react'
 
 const SearchMovie = ({searchTerm,setSearchTerm,searchMovies}) => {
-  const inputRef = useRef(null)
   return (
     <div className='search'>
-        <input ref={inputRef} type="text" placeholder='Search for Movies' value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)} />
+        <input type="text" placeholder='Search for Movies' value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)} />
         <img src={SearchIcon} alt="" onClick={()=>searchMovies(searchTerm)} />
     </div>
   )
