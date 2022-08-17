70d4217e1cd7d547105abf497a4ed2c0a552fe34
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-08-17 13:14:49 +0100

    fix errors for image movie

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -7,7 +7,7 @@ const Movie = ({movie}) => {
         <p>{movie.Year}</p>
       </div>
       <div>
-        <img src={movie.image} alt="" />
+        <img src={movie.Poster!=='N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt="" />
       </div>
       <div>
         <span>{movie.Type}</span>
