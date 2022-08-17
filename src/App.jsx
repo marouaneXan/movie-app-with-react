fbd088ce6f205e917260b315964e819767e9dd1d
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-08-17 12:58:34 +0100

    git movies by title

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import './App.css';
+import {useState,useEffect} from 'react' 
 import SearchMovie from './components/SearchMovie'
+import axios from 'axios';
+const api_url='http://www.omdbapi.com?apikey=985fa6c2'
 function App() {
+  const [movies, setMovies] = useState([])
+  const searchMovies=async (title)=>{
+    const res=await axios(`${api_url}&s=${title}`)
+    setMovies(res.data.Search)
+  }
+  useEffect(()=>{
+    searchMovies('spiderman')
+  },[])
+
   return (
     <div className="app">
       <h1>MovieLand</h1>
       <SearchMovie/>
+      {movies.map((movie)=>(
+        <>
+        <h3>{movie.Title}</h3>
+        <p>{movie.Year}</p>
+        <p>{movie.Type}</p>
+        </>
+      ))}
     </div>
   );
 }
